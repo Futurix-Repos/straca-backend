5fd2107baaed69c83aca785ab9bd96709069fef0
Anicet21 <anicetafagnon47@gmail.com>
2026-09-09 23:08:31 +0100

    offline + voucher number

--- playground-2.mongodb.js
+++
@@ -1,8 +0,0 @@
-/* global use */
-// MongoDB Playground
-// Use Ctrl+Space inside a snippet or a string literal to trigger completions.
-
-// The current database to use.
-use("straca");
-
-db.orders.find({}, { reference: 1, status: 1 }).limit(10);
